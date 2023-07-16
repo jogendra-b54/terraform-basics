c52f655ecdc95491e9ece86bc2a63fc3809fae2b
jogendra-b54 <jpatro.kumar@gmail.com>
2023-07-16 22:09:52 +0530

    Auto push

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,12 @@ variable "sample" {
 output "sample-op" {
     value = var.sample
 }
+
+output "sample-var-op" {
+    value = "value of the variable is ${var.sample-op}"
+}
+
+# A variable can be accessed directly by caling it as var.variableName but if you'r accessing in between a 
+#set of String ,then you need to enclose them in quotes and it should be reffered using ${var.varName}
+
+# in terraform there is no concept of Single quotes
