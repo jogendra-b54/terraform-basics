64433cba8d529e60764c01d5c3284e717b238b96
jogendra-b54 <jpatro.kumar@gmail.com>
2023-07-17 12:17:58 +0530

    Auto push

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -14,3 +14,19 @@ output "sample-var-op" {
 #set of String ,then you need to enclose them in quotes and it should be reffered using ${var.varName}
 
 # in terraform there is no concept of Single quotes
+
+#List variable
+variable "example_list" {
+  default = [
+        "AWS",
+        "DevOps",
+          55 ,
+        "Trainer",
+        "Terraform"
+  ]
+}
+#You also need to try to understand the data in the List Object doesn't necessary to be of the same data type
+
+output "example_list_op" {
+  value = "Welcome to DevOps with AWS  ${var.example_list[1]} Training with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current Topic is ${var.example_list[4]}"
+}
