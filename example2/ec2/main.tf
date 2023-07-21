8d444741c730a0e744c42bd83f6cf17f173df20e
jogendra-b54 <jpatro.kumar@gmail.com>
2023-07-21 22:17:53 +0530

    Auto push

---
+++ example2/ec2/main.tf
@@ -0,0 +1,35 @@
+# Creates EC2
+resource "aws_instance" "sample" {
+  ami           = "ami-0c1d144c8fdd8d690"
+  instance_type = "t3.micro"
+  vpc_security_group_ids  = ["aws_security_group.allow_all.id"]
+  tags = {
+    Name = "terraformServer"
+  }
+} 
+
+# Creates a security Group EC2
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]     
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
