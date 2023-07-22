1845656fc9da090cd0932dec08f567a1ede495f8
jogendra-b54 <jpatro.kumar@gmail.com>
2023-07-22 18:12:17 +0530

    Auto push

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -9,6 +9,6 @@ data "aws_ami" "image" {
   # name_regex = "DevOps-LabImage-Centos7"
   #owners =  ["self"]   # self because the AMI is created as visibilty =private
 
-    owners = ["355449129"]
+    #owners = ["355449129"]
 
   }
