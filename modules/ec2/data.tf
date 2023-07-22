60313f0315eeca0a6e5a295de3b084fb5c058762
jogendra-b54 <jpatro.kumar@gmail.com>
2023-07-22 18:11:02 +0530

    Auto push

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,5 +1,14 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex = "Jogendra-learning-image"
-  owners =  ["self"]
+
+  #name_regex = "Jogendra-learning-image"
+
+  name_regex= "DevOps-LabImage-CentOS7"
+
+  # you can also give any public image 
+  # name_regex = "DevOps-LabImage-Centos7"
+  #owners =  ["self"]   # self because the AMI is created as visibilty =private
+
+    owners = ["355449129"]
+
   }
