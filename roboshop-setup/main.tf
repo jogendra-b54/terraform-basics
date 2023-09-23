85d629dba692292d52a4b4ce89c337cd404fa749
jogendra-b54 <jpatro.kumar@gmail.com>
2023-09-23 15:40:54 +0530

    Auto push

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -4,3 +4,23 @@ module "frontend" {
   APP_VERSION = "0.0.3"  
 
 }
+# module "mongodb" {
+#   source = "./ec2"
+#   COMPONENT =  "mongodb"
+#   APP_VERSION = "0.0.2"  
+
+# }
+
+# module "cart" {
+#   source = "./ec2"
+#   COMPONENT =  "cart"
+#   APP_VERSION = "0.0.2"  
+
+# }
+
+# module "catalogue" {
+#   source = "./ec2"
+#   COMPONENT =  "catalogue"
+#   APP_VERSION = "0.0.2"  
+
+# }
---
+++ roboshop-setup/vars.tf
@@ -0,0 +1,37 @@
+# MAP variable
+
+variable "COMPONENT" {
+    default = {
+        mongodb = {
+            APP_VERSION = "null"
+        },
+        catalogue = {
+            APP_VERSION ="0.0.8"
+        },
+        redis = {
+            APP_VERSION ="null"
+        },
+        user = {
+            APP_VERSION ="0.0.5"
+        },
+        cart = {
+            APP_VERSION ="0.0.4"
+        },
+        mysql = {
+            APP_VERSION ="null"
+        },
+        shipping = {
+            APP_VERSION ="0.0.4"
+        },
+        rabbitmq = {
+            APP_VERSION ="null"
+        },
+        payment = {
+            APP_VERSION ="0.0.3"
+        },
+        frontend = {
+            APP_VERSION ="0.0.3"
+        },
+    }
+
+}
